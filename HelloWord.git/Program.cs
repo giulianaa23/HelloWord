85488a5aff75c7485ac9d881fe472f47a60001ba
giulianaa23 <giulianacristea06@gmail.com>
2024-01-22 10:25:10 +0200

    initial project setup

---
+++ HelloWord.git/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti primul numar intreg:");
+        int numar1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduceti al doilea numar intreg:");
+        int numar2 = int.Parse(Console.ReadLine());
+
+        int suma = numar1 + numar2;
+
+        Console.WriteLine($"Rezultatul adunarii este: {suma}");
+    }
+}
+
---
+++ VisualStudio/Program.cs
@@ -0,0 +1,37 @@
+﻿ System;
+using System.Linq;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int YearOfBirth { get; set; }
+    public int Age => DateTime.Now.Year - YearOfBirth;
+
+   }  using
+
+
+   class Program
+  {  
+      static void Main ()
+     { 
+        Console.Write()
+        int n = int.Parse(Console.ReadLine());
+
+        Person[] people = new Person[n];
+
+        for (int i = 0,i < n;i++)
+        {
+            Console.WriteLine(i+1);
+            Console.WriteLine(FirstName);
+            string firstName = Console.ReadLine();
+
+
+            
+
+
+
+
+
+
+     
